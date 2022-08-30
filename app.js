404aaec0ee4b2f7b47cf041325a1e200fd1fe17e
ShravanJP <shravanjp8910@gmail.com>
2022-08-30 11:32:27 +0530

    resolved the bug: the public folder was not specified as static

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const socket = require("socket.io");
 
 const app = express();
-app.use(express.static("./"));
+app.use(express.static("public"));
 
 let port = 3000;
 let server = app.listen(port, ()=> {
