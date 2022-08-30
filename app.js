7bb5a8c0dd451e9cdf706d5e7bf67274d80d7263
ShravanJP <shravanjp8910@gmail.com>
2022-08-30 11:47:40 +0530

    added the logic to choose the port based on the production environment otherwise or port 3000

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const socket = require("socket.io");
 const app = express();
 app.use(express.static("public"));
 
-let port = 3000;
+let port = process.env.PORT || 3000;
 let server = app.listen(port, ()=> {
     console.log("listing to port "+port);
 })
